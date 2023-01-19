fedb25e7d1040847ac4ce5e3b0a8af3d75e32e73
Viviansol <viviansan93@gmail.com.br>
2023-01-19 14:48:41 -0300

    added templates

---
+++ cmd/web/middleware.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func (app *Config) SessionLoad(next http.Handler) http.Handler {
+
+	return app.Session.LoadAndSave(next)
+
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,7 +10,15 @@ func (app *Config) routes() http.Handler {
 
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
+	mux.Use(app.SessionLoad)
 	mux.Get("/", app.HomePage)
+	mux.Get("/login", app.LoginPage)
+	mux.Post("/login", app.PostLoginPage)
+	mux.Get("/logout", app.LoginPage)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/regist", app.PostRegisterPage)
+	mux.Get("/activate", app.ActiveAccount)
+
 	return mux
 
 }
